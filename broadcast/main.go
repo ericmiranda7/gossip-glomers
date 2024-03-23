baba5c6e8ca243b882926972a4b93e751ec0f967
Eric Miranda <ericmiranda7@gmail.com>
2024-03-23 11:27:30 +0530

    highly inefficient broadcast 3b complete

--- broadcast/main.go
+++ broadcast/main.go
@@ -4,11 +4,19 @@ import (
 	"encoding/json"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"sync"
 )
+
+type topologyMsg struct {
+	Type     string              `json:"type"`
+	Topology map[string][]string `json:"topology"`
+}
 
 func main() {
 	n := maelstrom.NewNode()
-	var storedMsgs []int
+	mu := sync.Mutex{}
+	storedMsgs := make(map[int]bool)
+	var neighbours []string
 
 	n.Handle("broadcast", func(message maelstrom.Message) error {
 		var body map[string]any
@@ -18,28 +26,66 @@ func main() {
 			return err
 		}
 
-		storedMsgs = append(storedMsgs, int(body["message"].(float64)))
+		// safely store
+		val := int(body["message"].(float64))
+		mu.Lock()
+		if _, exists := storedMsgs[val]; !exists {
+			storedMsgs[val] = true
+		}
+		mu.Unlock()
 
-		res := make(map[string]string)
-		res["type"] = "broadcast_ok"
-		return n.Reply(message, res)
+		// todo(): use src, dest
+
+		// send the message out only to non-SRC neighbours
+		broadcastMsg := make(map[string]any)
+		broadcastMsg["type"] = "broadcast"
+		broadcastMsg["message"] = val
+		for _, neighbour := range neighbours {
+			if neighbour != message.Src {
+				err := n.Send(neighbour, broadcastMsg)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		if message.Src[0] == 'c' {
+			res := make(map[string]string)
+			res["type"] = "broadcast_ok"
+			return n.Reply(message, res)
+		}
+		return nil
 	})
 
-	/*
-		{
-		  "type": "read_ok",
-		  "messages": [1, 8, 72, 25]
-		}
-	*/
 	n.Handle("read", func(message maelstrom.Message) error {
 		res := make(map[string]any)
 		res["type"] = "read_ok"
-		res["messages"] = storedMsgs
+
+		var msgsList []int
+		mu.Lock()
+		for k := range storedMsgs {
+			msgsList = append(msgsList, k)
+		}
+		mu.Unlock()
+
+		res["messages"] = msgsList
 
 		return n.Reply(message, res)
 	})
 
 	n.Handle("topology", func(message maelstrom.Message) error {
+		body := topologyMsg{}
+		if err := json.Unmarshal(message.Body, &body); err != nil {
+			return err
+		}
+
+		// 1. parse the topology (who can talk to who)
+		//		a. get topology object
+		//		b. get neighbours for current node
+		neighbours = body.Topology[n.ID()]
+
+		// 2. nodes can only talk to neighbours
+
 		res := make(map[string]any)
 		res["type"] = "topology_ok"
 		return n.Reply(message, res)
