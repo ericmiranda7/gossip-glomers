684a557c9b0db90ccbc57e648b3899e2234d1e0a
Eric Miranda <ericmiranda7@gmail.com>
2024-05-19 16:25:01 +0530

    solve #3d, simplify

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
-	"sync"
 	"time"
 )
 
@@ -15,9 +14,6 @@ type Server struct {
 	neighbourNodes []string
 	valStore       []int
 	valChan        chan int
-	msgChan        chan customMessage
-	processedMsgs  map[string]bool
-	processedMu    sync.Mutex
 }
 
 type topologyMsg struct {
@@ -45,14 +41,11 @@ func main() {
 		neighbourNodes: []string{},
 		valStore:       []int{},
 		valChan:        make(chan int, 10000),
-		msgChan:        make(chan customMessage, 10000),
-		processedMsgs:  make(map[string]bool),
-		processedMu:    sync.Mutex{},
 	}
 
 	// client handlers
 	s.n.Handle("topology", s.topologyHandler)
-	s.n.Handle("broadcast", s.recvBroadcast)
+	s.n.Handle("broadcast", s.broadcastHandler)
 	s.n.Handle("read", s.readHandler)
 
 	// background housekeepers
@@ -66,28 +59,12 @@ func main() {
 
 func (s *Server) processMessages() {
 	for {
-		select {
-		case v := <-s.valChan:
-			// msgStore updater
-			log.Println("ADDED", v)
-			s.valStore = append(s.valStore, v)
-		case m := <-s.msgChan:
-			go s.neighbourNotifier(m)
-		}
+		v := <-s.valChan
+		s.valStore = append(s.valStore, v)
 	}
 }
 
-func (s *Server) neighbourNotifier(msg customMessage) {
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second*2))
-	_, err := s.n.SyncRPC(ctx, msg.dest, msg.msgBody)
-	if err != nil {
-		log.Println("Some error", err)
-		go s.neighbourNotifier(msg)
-	}
-	cancel()
-}
-
-func (s *Server) recvBroadcast(msg maelstrom.Message) error {
+func (s *Server) broadcastHandler(msg maelstrom.Message) error {
 	body, err := extractBody(msg)
 	if err != nil {
 		return err
@@ -100,13 +77,6 @@ func (s *Server) recvBroadcast(msg maelstrom.Message) error {
 	case 'n':
 		// if node src
 		id = body.Mid
-		s.processedMu.Lock()
-		processed := s.processedMsgs[id]
-		s.processedMu.Unlock()
-		if processed {
-			log.Println("already processed", id, body.Message)
-			return s.n.Reply(msg, map[string]string{"type": "broadcast_fine", "mid": id})
-		}
 
 		err := s.n.Reply(msg, map[string]string{"type": "broadcast_fine", "mid": id})
 		if err != nil {
@@ -127,11 +97,7 @@ func (s *Server) recvBroadcast(msg maelstrom.Message) error {
 			return err
 		}
 	}
-	s.processedMu.Lock()
-	s.processedMsgs[id] = true
-	s.processedMu.Unlock()
 
-	log.Println("GOT", id, body.Message)
 	s.valChan <- body.Message // send msg for storage
 
 	body.AlreadyGot[s.n.ID()] = true
@@ -150,13 +116,24 @@ func (s *Server) notifyNeighbours(needToSendTo []string, body *messageBody) {
 	}
 
 	for _, destNode := range needToSendTo {
-		s.msgChan <- customMessage{
+		cmsg := customMessage{
 			id:      body.Mid,
 			n:       s.n,
 			dest:    destNode,
 			msgBody: messageBody{MsgType: "broadcast", Message: body.Message, Mid: body.Mid, AlreadyGot: body.AlreadyGot},
 		}
+		go s.neighbourNotifier(cmsg)
 	}
+}
+
+func (s *Server) neighbourNotifier(msg customMessage) {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Millisecond*500))
+	_, err := s.n.SyncRPC(ctx, msg.dest, msg.msgBody)
+	if err != nil {
+		log.Println("Some error", err)
+		go s.neighbourNotifier(msg)
+	}
+	cancel()
 }
 
 func (s *Server) topologyHandler(msg maelstrom.Message) error {
